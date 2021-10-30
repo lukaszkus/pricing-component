4f6572800a322bbc9b397a3bc837577d3bc93ebd
lukaszkus <kusiu@vp.pl>
2021-10-30 11:18:18 +0200

    js update

--- js/script.js
+++ js/script.js
@@ -4,16 +4,24 @@ const price = document.querySelectorAll('.box__price');
 const annualPrices = [199.99, 249.99, 399.99];
 const monthPrices = [19.99, 24.99, 39.99];
 
-togglePrice.addEventListener('change', function () {
-  for (let i = 0; i < monthPrices.length; i++) {
-    if (this.checked) {
-      price.forEach(el => {
-        el.innerHTML = "<span>&dollar;</span>" + monthPrices[i++] + "";
-      });
-    } else {
-      price.forEach(el => {
-        el.innerHTML = "<span>&dollar;</span>" + annualPrices[i++] + "";
-      });
-    }
+
+function annual() {
+  for (let i = 0; i < annualPrices.length; i++){
+    price.forEach(el => {
+      el.innerHTML = "<span>&dollar;</span> "+ annualPrices[i++] +" ";
+    });
   }
+}
+annual();
+
+function month() {
+  for (let i = 0; i < monthPrices.length; i++){
+    price.forEach(el => {
+    el.innerHTML = "<span>&dollar;</span> "+ monthPrices[i++] +" ";
+  });
+  }
+}
+
+togglePrice.addEventListener('change', function() {
+  this.checked ? month() : annual();
 });
